1067ec75fc2b6198bee6a24c89cad30a78cbb0d9
Ewennn <ewen.bosquet@gmail.com>
2025-03-24 23:19:44 +0100

    fix/ Password not hashed on user patch

--- apps/user-service/src/listeners/update-user-listener.ts
+++ apps/user-service/src/listeners/update-user-listener.ts
@@ -1,25 +1,35 @@
 import {RabbitmqRPCListener} from "./rabbitmq-listener";
-import {Users} from "../db/users";
+import {User, Users} from "../db/users";
+import bcrypt from "bcrypt";
 
 export class UpdateUserListener implements RabbitmqRPCListener {
 
     constructor(private readonly users: Users) {}
 
     onMessage(msg: string, reply: (response: object) => void): void {
-        const updated = JSON.parse(msg)
+        const { id, name, age, email, password, role } = JSON.parse(msg)
+
+        const updated: User = {
+            id,
+            name,
+            age,
+            email: email?.toLowerCase(),
+            password: bcrypt.hashSync(password, 10),
+            role: role,
+        }
 
         this.users.update(updated)
             .then(user => {
                 if (user.length === 0) {
-                    reply({
-                        error: `No user was updated with id ${updated.id}`,
+                    return reply({
+                        error: `No user was updated with id ${id}`,
                         code: 404,
                     })
                 }
 
                 if (user.length > 1) {
-                    reply({
-                        error: `Many users was updated with id ${updated.id}`,
+                    return reply({
+                        error: `Many users was updated with id ${id}`,
                         code: 409,
                     })
                 }
@@ -30,7 +40,7 @@ export class UpdateUserListener implements RabbitmqRPCListener {
             .catch(err => {
                 console.error(`Failed to update user: ${err}`)
                 reply({
-                    error: `Failed to update user with id ${updated.id}`,
+                    error: `Failed to update user with id ${id}`,
                     code: 500
                 })
             })
