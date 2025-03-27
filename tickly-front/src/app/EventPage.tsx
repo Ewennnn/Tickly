5ab6e34ea0127766367e6f76c1d56d52c939e4bd
Ewennn <ewen.bosquet@gmail.com>
2025-03-27 15:20:58 +0100

    feat/ Show message when no events are available

--- tickly-front/src/app/EventPage.tsx
+++ tickly-front/src/app/EventPage.tsx
@@ -121,87 +121,123 @@ export const EventsPage = () => {
             <main className="container mx-auto px-4 py-8">
                 <h1 className="text-3xl font-bold mb-8" style={{ color: primaryColor }}>Événements à venir</h1>
 
-                <div className="flex flex-wrap -mx-3">
-                    {events?.map((event) => (
-                        <div key={event.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-3 mb-6">
-                            <div className="bg-white rounded-xl shadow-md overflow-hidden h-full hover:shadow-lg transition-all duration-300 flex flex-col">
-                                {/* Image */}
-                                <div className="relative h-48 bg-gray-200">
-                                    <EventSlider images={event.images} />
-                                </div>
-
-                                <div className="p-6 flex flex-col flex-grow">
-                                    <h2 className="text-xl font-bold mb-2" style={{ color: primaryColor }}>{event.name}</h2>
-
-                                    <div className="mb-4 text-sm text-gray-600">
-                                        <div className="flex items-center mb-1">
-                                            <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor"
-                                                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                                      d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
-                                            </svg>
-                                            {formatDate(event.date)}
-                                        </div>
-                                        <div className="flex items-center">
-                                            <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor"
-                                                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                            </svg>
-                                            {event.location}
-                                        </div>
-                                        <div className="flex items-center">
-                                            <svg className="w-4 h-4 mr-2" aria-hidden="true" fill="none" strokeWidth='2' viewBox="0 0 24 24">
-                                                <path stroke="currentColor" d="M18.5 12A2.5 2.5 0 0 1 21 9.5V7a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v2.5a2.5 2.5 0 0 1 0 5V17a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1v-2.5a2.5 2.5 0 0 1-2.5-2.5Z"/>
-                                            </svg>
-                                            <span className="text-medium text-gray-700">
-                                                {event.seats} places
-                                            </span>
-                                        </div>
+                {/* Check if there are no events */}
+                {events === undefined ? (
+                    <div className="text-center text-gray-500">
+                        <p>Aucun événement</p>
+                    </div>
+                ) : (
+                    <div className="flex flex-wrap -mx-3">
+                        {events?.map((event) => (
+                            <div key={event.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-3 mb-6">
+                                <div className="bg-white rounded-xl shadow-md overflow-hidden h-full hover:shadow-lg transition-all duration-300 flex flex-col">
+                                    {/* Image */}
+                                    <div className="relative h-48 bg-gray-200">
+                                        <EventSlider images={event.images} />
                                     </div>
 
-                                    <div className="mb-4 flex-grow">
-                                        <p className="text-sm text-gray-700">{event.description}</p>
-                                    </div>
+                                    <div className="p-6 flex flex-col flex-grow">
+                                        <h2 className="text-xl font-bold mb-2" style={{ color: primaryColor }}>
+                                            {event.name}
+                                        </h2>
 
-                                    {/* Places availability */}
-                                    <div className="mb-6">
-                                        {event.remainingSeats !== undefined ? (
-                                            <>
-                                                <div className="flex justify-between text-sm mb-1">
-                                                    <span className="text-medium text-gray-700">Places restantes</span>
-                                                    <span className="font-medium text-gray-700">
-                                            {event.remainingSeats} / {event.seats}
-                                        </span>
-                                                </div>
-                                                <div className="w-full bg-gray-200 rounded-full h-2.5">
-                                                    <div
-                                                        className={`h-2.5 rounded-full ${calculateProgressColor(event.seats, event.remainingSeats)}`}
-                                                        style={{ width: `${(event.remainingSeats / event.seats) * 100}%` }}
-                                                    ></div>
-                                                </div>
-                                            </>
-                                        ) : (
-                                            <div className="text-sm text-red-500">
-                                                Le service de tickets est inaccessible
+                                        <div className="mb-4 text-sm text-gray-600">
+                                            <div className="flex items-center mb-1">
+                                                <svg
+                                                    className="w-4 h-4 mr-2"
+                                                    fill="none"
+                                                    stroke="currentColor"
+                                                    viewBox="0 0 24 24"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                >
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        strokeWidth="2"
+                                                        d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                                                    ></path>
+                                                </svg>
+                                                {formatDate(event.date)}
                                             </div>
-                                        )}
-                                    </div>
+                                            <div className="flex items-center">
+                                                <svg
+                                                    className="w-4 h-4 mr-2"
+                                                    fill="none"
+                                                    stroke="currentColor"
+                                                    viewBox="0 0 24 24"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                >
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        strokeWidth="2"
+                                                        d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                                                    ></path>
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        strokeWidth="2"
+                                                        d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                                                    ></path>
+                                                </svg>
+                                                {event.location}
+                                            </div>
+                                            <div className="flex items-center">
+                                                <svg
+                                                    className="w-4 h-4 mr-2"
+                                                    aria-hidden="true"
+                                                    fill="none"
+                                                    strokeWidth="2"
+                                                    viewBox="0 0 24 24"
+                                                >
+                                                    <path
+                                                        stroke="currentColor"
+                                                        d="M18.5 12A2.5 2.5 0 0 1 21 9.5V7a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v2.5a2.5 2.5 0 0 1 0 5V17a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1v-2.5a2.5 2.5 0 0 1-2.5-2.5Z"
+                                                    />
+                                                </svg>
+                                                <span className="text-medium text-gray-700">
+                                        {event.seats} places
+                                    </span>
+                                            </div>
+                                        </div>
 
-                                    <CustomButton
-                                        onClick={toggleLoginModal}
-                                        primary={true}
-                                        fullWidth={true}
-                                    >
-                                        S'inscrire à l'événement
-                                    </CustomButton>
+                                        <div className="mb-4 flex-grow">
+                                            <p className="text-sm text-gray-700">{event.description}</p>
+                                        </div>
+
+                                        {/* Places availability */}
+                                        <div className="mb-6">
+                                            {event.remainingSeats !== undefined ? (
+                                                <>
+                                                    <div className="flex justify-between text-sm mb-1">
+                                                        <span className="text-medium text-gray-700">Places restantes</span>
+                                                        <span className="font-medium text-gray-700">
+                                                {event.remainingSeats} / {event.seats}
+                                            </span>
+                                                    </div>
+                                                    <div className="w-full bg-gray-200 rounded-full h-2.5">
+                                                        <div
+                                                            className={`h-2.5 rounded-full ${calculateProgressColor(event.seats, event.remainingSeats)}`}
+                                                            style={{ width: `${(event.remainingSeats / event.seats) * 100}%` }}
+                                                        ></div>
+                                                    </div>
+                                                </>
+                                            ) : (
+                                                <div className="text-sm text-red-500">
+                                                    Le service de tickets est inaccessible
+                                                </div>
+                                            )}
+                                        </div>
+
+                                        <CustomButton onClick={toggleLoginModal} primary={true} fullWidth={true}>
+                                            S'inscrire à l'événement
+                                        </CustomButton>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
+                        ))}
+                    </div>
+                )}
             </main>
 
             {/* Footer */}
